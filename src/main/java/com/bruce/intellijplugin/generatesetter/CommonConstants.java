22fca581b1e5065367d13452eea99967642eb7de
葛俊 <gejun123456@gmail.com>
2018-10-26 21:31:06 +0800

    fix name for method name.

--- src/main/java/com/bruce/intellijplugin/generatesetter/CommonConstants.java
+++ src/main/java/com/bruce/intellijplugin/generatesetter/CommonConstants.java
@@ -18,5 +18,5 @@ package com.bruce.intellijplugin.generatesetter;
  * @author bruce ge
  */
 public class CommonConstants {
-    public static final String GENERATE_SETTER_METHOD = "generate all setter";
+    public static final String GENERATE_SETTER_METHOD = "Generate all setter";
 }
