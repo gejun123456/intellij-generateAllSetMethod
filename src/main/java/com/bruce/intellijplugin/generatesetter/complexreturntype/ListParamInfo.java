7aa4168d9a01af6d0028610d65401b4d9fbff326
葛俊 <gejun123456@gmail.com>
2019-03-05 13:39:38 +0800

    update all copyRights.

--- src/main/java/com/bruce/intellijplugin/generatesetter/complexreturntype/ListParamInfo.java
+++ src/main/java/com/bruce/intellijplugin/generatesetter/complexreturntype/ListParamInfo.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017-2018, bruce.ge.
+ *  Copyright (c) 2017-2019, bruce.ge.
  *    This program is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU General Public License
  *    as published by the Free Software Foundation; version 2 of
