eefbae48181f6a5b86709bf3692f35cda5111e71
葛俊 <gejun123456@gmail.com>
2019-03-22 08:43:53 +0800

    fix for function getter setter mapping not work.

---
+++ src/main/java/com/bruce/intellijplugin/generatesetter/actions/GenerateSetterGetterFromFunctionAction.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2017-2019, bruce.ge.
+ *    This program is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU General Public License
+ *    as published by the Free Software Foundation; version 2 of
+ *    the License.
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *    GNU General Public License for more details.
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program;
+ */
+
+package com.bruce.intellijplugin.generatesetter.actions;
+
+import com.bruce.intellijplugin.generatesetter.GenerateAllHandlerAdapter;
+
+/**
+ * @author bruce ge
+ */
+public class GenerateSetterGetterFromFunctionAction extends GenerateAllSetterBase {
+    public GenerateSetterGetterFromFunctionAction() {
+        super(new GenerateAllHandlerAdapter(){
+            @Override
+            public boolean isFromMethod() {
+                return true;
+            }
+        });
+    }
+}
