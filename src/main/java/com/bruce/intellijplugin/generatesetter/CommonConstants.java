ac97ea451ad87ad0be6ce964c42a16cd156e7b57
wenquan <wb-wq942540@alibaba-inc.com>
2021-09-28 16:31:42 +0800

    add generate get

--- src/main/java/com/bruce/intellijplugin/generatesetter/CommonConstants.java
+++ src/main/java/com/bruce/intellijplugin/generatesetter/CommonConstants.java
@@ -23,6 +23,7 @@ public class CommonConstants {
     public static final String GENERATE_BUILDER_METHOD = "Generate builder chain call";
     public static final String GENERATE_SETTER_METHOD_NO_DEAULT_VALUE = "Generate all setter no default value";
     public static final String ASSERTALLPROPS = "Assert all getters";
+    public static final String GENERATE_GETTER_METHOD = "Generate all getter";
     public static final String GENERATE_CONVERTER_FROM_METHOD = "Generate setter getter converter";
 
     public static final String BUILDER_METHOD_NAME = "builder";
